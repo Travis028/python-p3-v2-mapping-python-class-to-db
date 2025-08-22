c245c61ec1e433e7a4560152951b73d24a894307
waynetravis <waynetavis123@gmail.com>
2025-08-22 16:18:51 +0300

    commit changes made

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,55 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        """Create a new table to persist Department instances"""
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the departments table"""
+        sql = "DROP TABLE IF EXISTS departments;"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """Insert a new row with current instance values"""
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        """Initialize and save a new Department instance"""
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        """Update the table row for current instance"""
+        sql = """
+            UPDATE departments 
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row for current instance"""
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
